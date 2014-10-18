7691a543110ee97ff6d8ece5869496d14d3dc01e
Nan Davenport <nannigan@sonic.net>
2014-10-18 00:49:45 +0000

    finish stripe setup

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,28 +1,31 @@
 class EnrollmentsController < ApplicationController
 	 before_action :authenticate_user!
 	def create
-		 current_user.enrollments.create(:course => current_course)
-		
 
+  if current_course.premium?  
 		 # Amount in cents
-  @amount = (current_course.cost * 100).to_i
+    @amount = (current_course.cost * 100).to_i
 
-  customer = Stripe::Customer.create(
-    :email => current_user.email,
-    :card  => params[:stripeToken]
-  )
+    customer = Stripe::Customer.create(
+      :email => current_user.email,
+      :card  => params[:stripeToken]
+    )
 
-  charge = Stripe::Charge.create(
-    :customer    => customer.id,
-    :amount      => @amount,
-    :description => 'Flixter Paid Course customer',
-    :currency    => 'usd'
-  )
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Flixter Paid Course customer',
+      :currency    => 'usd'
+    )
+  end
+  
+   current_user.enrollments.create(:course => current_course)
 	 redirect_to course_path(current_course)
-	rescue Stripe::CardError => e
-	  flash[:error] = e.message
-	  redirect_to root_path
-	end
+  	
+    rescue Stripe::CardError => e
+  	  flash[:error] = e.message
+  	  redirect_to root_path
+  	end
 
 	private
 
